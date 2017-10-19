77baf0478ec0c0b9052e84c92ea1a4574952b72f
Malo Skrylevo <majioa@yandex.ru>
2017-10-19 15:33:16 +0300

    Commented out excplicit import of CustomEvent

--- src/utils/dispatch-event.js
+++ src/utils/dispatch-event.js
@@ -1,6 +1,10 @@
-import CustomEvent from 'custom-event';
-
 /**
+ * to use CustomEvent you have to require/import CustomEvent from custom-event or
+ * custom-event-polyfill packages in case it is not defined explicitly.
+ * Example:
+ *
+ * `import CustomEvent from 'custom-event';`
+ *
  * dispatch custom events
  *
  * @param  {element} el         slideshow element
